b78c25d34c8513482c349d93ac358f2cb59e4cb7
M3D5 <pablohdzjr@gmail.com>
2020-08-11 14:09:29 -0500

    created routes and models directory, with Workout schema

---
+++ models/fitnessData.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema( {
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter type of workout"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter workout name"
+                },
+            duration: {
+                type: Number,
+                required: "Enter duration"
+            },
+            weight: {
+                type: Number,
+                required: "Enter weight"
+            },
+            reps: {
+                type: Number,
+                required: "Enter number of reps"
+            },
+            sets: {
+                type: Number,
+                required:  "Enter number of sets"
+            }
+        }
+    ]
+
+})
+
+const 
+
+module.exports = Workout;
