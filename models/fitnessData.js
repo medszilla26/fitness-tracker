d3ec549b06b684a2e4e536c46ea9843daeddb402
M3D5 <pablohdzjr@gmail.com>
2020-08-11 14:12:01 -0500

    forgot to hit save from previous push

--- models/fitnessData.js
+++ models/fitnessData.js
@@ -2,44 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema( {
-    day: {
-        type: Date,
-        default: Date.now
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter type of workout",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter workout name",
+      },
+      duration: {
+        type: Number,
+        required: "Enter duration",
+      },
+      weight: {
+        type: Number,
+        required: "Enter weight",
+      },
+      reps: {
+        type: Number,
+        required: "Enter number of reps",
+      },
+      sets: {
+        type: Number,
+        required: "Enter number of sets",
+      },
     },
-    exercises: [
-        {
-            type: {
-                type: String,
-                trim: true,
-                required: "Enter type of workout"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Enter workout name"
-                },
-            duration: {
-                type: Number,
-                required: "Enter duration"
-            },
-            weight: {
-                type: Number,
-                required: "Enter weight"
-            },
-            reps: {
-                type: Number,
-                required: "Enter number of reps"
-            },
-            sets: {
-                type: Number,
-                required:  "Enter number of sets"
-            }
-        }
-    ]
+  ],
+});
 
-})
-
-const 
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Workout;
